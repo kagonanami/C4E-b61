7def3b88c77782048060de2be482640759f713b2
unknown <kagonanami@gmail.com>
2022-05-06 16:54:51 +0700

    2nd commit

--- homework/b61.js
+++ homework/b61.js
@@ -1,7 +1,12 @@
 let firstPlayer = prompt ("Please enter your name");
-let yourHeight = prompt("Please enter your height");
-let subPlayer = prompt ("Please enter your partner's height");
-let yourWeight = prompt("Please enter your weight")
+let yourHeight = prompt("Please enter your height (cm)");
+let subPlayer = prompt ("Please enter your partner's height (cm)");
+let yourWeight = prompt("Please enter your weight");
 
 // note
+
+yourHeight = Number(yourHeight);
+subPlayer = Number(subPlayer)
+let totalHeight = yourHeight + subPlayer
+console.log(totalHeight);
 
