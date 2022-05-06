a06380e8530b0bc9ac391a88ac719202b34ad84d
unknown <kagonanami@gmail.com>
2022-05-06 17:38:44 +0700

    5th commit

--- homework/b61.js
+++ homework/b61.js
@@ -2,9 +2,9 @@
 let firstPlayer = prompt ("Please enter your name");
 let yourHeight = prompt("Please enter your height (cm)");
 let subPlayer = prompt ("Please enter your partner's height (cm)");
-let yourWeight = prompt("Please enter your weight");
+let yourWeight = prompt("Please enter your weight (kg)");
 
-// b, c
+// b
 // note
 yourHeight = Number(yourHeight);
 subPlayer = Number(subPlayer);
@@ -22,3 +22,15 @@ yourWeight = Number(yourWeight);
 yourHeight = Number(yourHeight);
 let BMI = yourWeight / (yourHeight * 2);
 console.log(BMI);
+
+//c
+yourHeight = Number(yourHeight);
+subPlayer = Number(subPlayer);
+console.log(`Chiều cao của bạn đã nhập là ${yourHeight} , chiều cao của ny bạn đã nhập là ${subPlayer}`);
+
+heigthDifference = Number(heigthDifference);
+totalHeight = Number(totalHeight);
+console.log(`Độ chênh lệch chiều cao của 2 người là ${heigthDifference}, tổng chiều cao 2 người là ${totalHeight}`);
+
+BMI = Number(BMI);
+console.log(`Giá trị BMI sau khi tính toán là: ${BMI}`);
